06322729f2d0bc9ae29f3c929fd25d706b053a55
Serp77 <60550486+Serp77@users.noreply.github.com>
2023-09-16 08:30:25 +0300

    Add files via upload

---
+++ DZ1/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Input first num: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second num: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if(num1 > num2)
+//     Console.WriteLine($"{num1} > {num2}");
+// else
+//     Console.WriteLine($"{num2} > {num1}");
+
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Input first num: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second num: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input third num: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// if(num2 > num1 & num2 > num3){
+//     Console.WriteLine($"Max {num2}");
+// }
+// else if(num1 > num2 & num1 > num3){
+//     Console.WriteLine($"Max {num1}");
+// }
+// else 
+//     Console.WriteLine($"Max {num3}");
+
+          
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Input first num: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num % 2 == 0)
+//        Console.WriteLine($"{num} yes");
+// else       
+//        Console.WriteLine($"{num} no");
+
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Input num: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= n; i++){
+//      if (i % 2 == 0){
+//     Console.Write($"{i}, ");
+//      }
+// }
+
+
+
+
